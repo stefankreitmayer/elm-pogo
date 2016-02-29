ddb039d760a1da8109af5c10f3128c6bb0279520
stefankreitmayer <stefankreitmayer@gmail.com>
2016-02-29 10:41:41 +0100

    animate hand

--- punk.elm
+++ punk.elm
@@ -1,14 +1,19 @@
+import String exposing (concat)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
+import Time exposing (every)
 
 main =
+  Signal.map scene (every 0.02)
+
+scene t =
   svg [ version "1.1", x "0", y "0", viewBox "0 0 400 400" ]
     [
       definitions
       , background
-      , hand
       , hair
       , mouth
+      , hand t
     ]
 
 definitions =
@@ -28,10 +33,20 @@ hair =
     , Svg.Attributes.style "fill:url(#grad1)"
   ] []
 
-hand =
-  Svg.path [
-    fill "#ffd5d5", d "M 149,61 C 149,23 129,31 127,61 126,89 126,106 125,142 101,124 71.5,137 60,142 57.7,89 57.6,62 46.8,62 34.8,64 40,106 40,159 40,195 54.6,230 96,230 137,230 151,212 151,177 151,124 150,124 149, 61 Z"
-  ] []
+hand t =
+  let
+    sine = (sin (t/90)) * 0.5 + 0.5
+    phase = sine ^ 0.6
+    scaleX = (1-phase) * 0.2 + 0.8
+    scaleY = phase * 0.3 + 0.6
+    translateX = (phase) * 20
+    translateY = (1.0-phase) * 100
+  in
+    Svg.path [
+      fill "#ffd5d5"
+      , d "M 149,61 C 149,23 129,31 127,61 126,89 126,106 125,142 101,124 71.5,137 60,142 57.7,89 57.6,62 46.8,62 34.8,64 40,106 40,159 40,195 54.6,230 96,230 137,230 151,212 151,177 151,124 150,124 149, 61 Z"
+      , String.concat ["translate(", (toString translateX) ,",", (toString translateY), ") scale(", (toString scaleX) ,",", (toString scaleY), ")"] |> transform
+    ] []
 
 mouth =
   Svg.path [

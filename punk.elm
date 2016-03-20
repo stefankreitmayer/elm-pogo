754662082865860c79568022898ba5e56ab5be31
stefankreitmayer <stefankreitmayer@gmail.com>
2016-03-20 19:48:57 +0100

    make it just PERFECT

--- punk.elm
+++ punk.elm
@@ -4,7 +4,7 @@ import Svg.Attributes exposing (..)
 import Time exposing (every)
 
 main =
-  Signal.map scene (every 25)
+  Signal.map scene (every 10)
 
 scene t =
   svg [ version "1.1", x "0", y "0", viewBox "0 0 400 400" ]
@@ -37,34 +37,45 @@ headRotation t =
                   , (((t + msPerCycle/4 |> floor) % (floor (msPerCycle * 2)) |> toFloat) / msPerCycle * 20 - 20 |> abs) - 10 |> toString
                   , ")"]
 
+slowVertical t =
+  around t -100 100 1
+
+compoundTransform transforms =
+  String.join " " transforms |> transform
+
+svgTransform command x y =
+  String.concat [ command, "(", (toString x), ",", (toString y), ")" ]
+
 hair t =
   let
-    startEndX = (around t 363 3 1.0) |> toString
-    startEndY = (bounce t 194 30 1.2) |> toString
+    startEndX = around t 113 3 1.0
+    startEndY = bounce t 6 30 1.2
+    tipX = around t -60 25 1.2
+    tipY = bounce t (around t 123 20 0.7) 25 1.2
   in
     Svg.path [
       fill "#00AD00"
       , d (String.join " "
         [ "M"
-        , startEndX
-        , startEndY
+        , startEndX |> toString
+        , startEndY |> toString
         , "C"
-        , (around t 370 0 3) |> toString--
-        , (bounce t 270 0 3) |> toString
-        , (around t 195 50 1.2) |> toString--
-        , (bounce t 159 40 1.3) |> toString
-        , (around t 191 25 1.2) |> toString--tip
-        , (bounce t (around t 323 20 0.7) 25 1.2) |> toString
-        , (around t 89 50 1.2) |> toString--
-        , (bounce t (around t 113 17 2) 15 1.6) |> toString
-        , (around t 352 10 0.9) |> toString--
-        , (bounce t 74.8 20 1.4) |> toString
-        , startEndX
-        , startEndY
+        , around t 120 0 3 |> toString
+        , bounce t 70 0 3 |> toString
+        , around t -55 50 1.2 |> toString
+        , bounce t -40 40 1.3 |> toString
+        , tipX |> toString
+        , tipY |> toString
+        , around t -160 50 1.2 |> toString
+        , bounce t (around t -90 17 2) 15 1.6 |> toString
+        , around t 100 10 0.9 |> toString
+        , bounce t -125 20 1.4 |> toString
+        , startEndX |> toString
+        , startEndY |> toString
         , "Z"
         ])
       , Svg.Attributes.style "fill:url(#grad1)"
-      , headRotation t |> transform
+      , compoundTransform [ headRotation t, svgTransform "translate" 250 (170 + (slowVertical t)) ]
     ] []
 
 around t center fluctuation frequency =
@@ -75,20 +86,20 @@ bounce t center fluctuation frequency =
 
 hand t =
   let
-    scaleX = around t 0.8 -0.2 1.2 |> toString
-    scaleY = around t 0.6 0.3 1.2 |> toString
-    translateX = around t 0 20 1.2 |> toString
-    translateY = around t 0 -100 1.2 |> toString
+    scaleX = around t 0.8 -0.2 1.2
+    scaleY = around t 0.6 0.3 1.2
+    translateX = around t 0 20 1.2
+    translateY = around t 0 -100 1.2
   in
     Svg.path [
       fill "#ffd5d5"
       , d "M 149,61 C 149,23 129,31 127,61 126,89 126,106 125,142 101,124 71.5,137 60,142 57.7,89 57.6,62 46.8,62 34.8,64 40,106 40,159 40,195 54.6,230 96,230 137,230 151,212 151,177 151,124 150,124 149, 61 Z"
-      , String.concat ["translate(", translateX ,",", translateY, ") scale(", scaleX ,",", scaleY, ")"] |> transform
+      , compoundTransform [headRotation t, svgTransform "translate" translateX translateY, svgTransform "scale" scaleX scaleY ]
     ] []
 
 mouthX1 t = (around t 258 -5 0.2)
 mouthX2 t = (around t 316 6 0.2)
-mouthY t = (bounce t 307 40 1.2)
+mouthY t = (bounce t (277 + (slowVertical t)) 40 1.2)
 
 mouth t =
   let
@@ -96,7 +107,7 @@ mouth t =
     y1 = mouthY t |> toString
     x2 = mouthX2 t |> toString
     y2 = mouthY t |> toString
-    bottomY = (bounce t 324 40 1.2) |> toString
+    bottomY = (mouthY t) + 20 |> toString
   in
     Svg.path [
       fill "#eeeeee"
@@ -113,9 +124,9 @@ mouth t =
         , y2
         , x2
         , y2
-        , (around t 311 0 1.2) |> toString--
+        , (around t 321 0 1.2) |> toString--
         , bottomY
-        , (around t 289 10 0.13) |> toString--
+        , (around t 299 10 0.13) |> toString--
         , bottomY
         , (around t 266 20 0.13) |> toString--
         , bottomY
@@ -135,21 +146,22 @@ moek t freqX freqY =
   let
     x = around t (noseX t) 56 freqX
     y = (mouthY t) - 13 - (around t 0 60 freqY |> abs)
+    radius = around t 2.6 1.6 (freqX - freqY)
   in
     circle
       [ cx (toString x)
       , cy (toString y)
-      , r "3"
+      , r (toString radius)
       , fill "#ff8800"
       , headRotation t |> transform
       ] []
 
 allMoek t =
-  [ moek t 1.1 2.5
-  , moek t 1.3 2.4
-  , moek t 1.6 2.3
-  , moek t 1.4 2.2
-  , moek t 1.5 2.1
+  [ moek t 1.1 0.5
+  , moek t 1.3 0.4
+  , moek t 0.6 1.3
+  , moek t 0.4 1.2
+  , moek t 0.5 1.1
   ]
 
 eyes t =
